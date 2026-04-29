0a71401f30681c7431d61fad0d396d9a3e617558
Deni Yusup <deni.yusup@bekaert.com>
2026-04-29 13:55:28 +0700

    update transcation history

--- spart/resources/views/transactions/create.blade.php
+++ spart/resources/views/transactions/create.blade.php
@@ -271,7 +271,7 @@
         if (window.TomSelect) {
             new TomSelect('#user_id_select', {
                 create: false,
-                sortField: 'text',
+                sortField: null, // Disable sorting, keep Blade/Controller order
                 placeholder: 'Cari nama user...'
             });
         }
